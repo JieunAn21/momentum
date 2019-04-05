3a0189ca8c72662a923e8d44cb2059239221bf54
jean <jeahn@bimpeers.com>
2019-04-05 11:11:02 +0900

    close #6 위치/날씨 표기

---
+++ weather.js
@@ -0,0 +1,56 @@
+const weatherSpan = document.querySelector(".js-weather");
+
+const API_KEY = "f71b8c12b497fee429558e628e0063be";
+const COORDS_LS = "coords";
+
+function getWeather(lat, lon) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        const temp = json.main.temp;
+        const place = json.name;
+        weatherSpan.innerText = `${ place } @ ${ temp }`;
+    });
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS_LS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    }
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+    console.log("can't access geo location");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS_LS);
+    if (loadedCoords === null) {
+        askForCoords();
+    } else {
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
